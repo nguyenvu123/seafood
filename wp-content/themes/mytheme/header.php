c1a0bb35a85bf8aa179faa782476f248087899fc
nguyen.vu <vu.nguyen@weareathena.com>
2018-03-15 17:36:59 +0700

    cd

---
+++ wp-content/themes/mytheme/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+ <?php wp_enqueue_style ('theme-style', get_template_directory_uri().'/source/css/main.css');  ?>  
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div id="wrapper" class="hfeed">
+<header id="header" role="banner">
+	<?php var_dump(get_template_directory_uri()); ?>
+	<div class="row">
+		<div class="col-3 logo">
+			<img src="">
+		</div>
+		<div class="col-7 menu">
+			<?php wp_nav_menu("main-menu"); ?>
+		</div>	
+	</div>
+</header>
+<div id="container">
---
+++ wp-content/themes/mytheme/pages/tpl-home.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: home page
+*/
+?>
+<?php get_header(); ?>
+
+	<div>
+		 <?php echo __('home page title vusa hhhh', "html") ?> 
+
+	</div>
+
+<?php get_footer(); ?>
