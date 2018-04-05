4d6beb810b94b71bd95eb521661fa64de4357d22
nguyen.vu <vu.nguyen@weareathena.com>
2018-04-05 16:28:07 +0700

    xoa het

--- wp-content/themes/mytheme/pages/tpl-home.php
+++ wp-content/themes/mytheme/pages/tpl-home.php
@@ -7,12 +7,6 @@ Template Name: home page
 
 	<div>
 		 <?php echo __('home page', "html") ?> 
-		 <h1>master</h1>   
-		 <h1>dev1</h1>
-	</div>
-	<div class="dev1-sua">
-	</div>
-	<div class="new-mới">
-		
+	
 	</div>
 <?php get_footer(); ?>
