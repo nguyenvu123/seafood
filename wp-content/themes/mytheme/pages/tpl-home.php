47eb93a6b3bd30d58dfd0171003c6f9cead4ef2f
nguyen.vu <vu.nguyen@weareathena.com>
2018-04-06 11:56:56 +0700

    add

--- wp-content/themes/mytheme/pages/tpl-home.php
+++ wp-content/themes/mytheme/pages/tpl-home.php
@@ -6,14 +6,10 @@ Template Name: home page
 <?php get_header(); ?>
 
 	<div>
-		 <?php echo __('home page', "html") ?> 
+		 <?php echo __('home page', "html") ?> \
+		 <h1>hgfjghfj</h1>
 	
 	</div>
-	<div class="dev1">
-		
-	</div>
-	<div class="dev2">
-		
-	</div>
+	
 
 <?php get_footer(); ?>
